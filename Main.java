0b68c1244172669093ac5cd0e790470214fe61c9
arch0047 <48592482+arch0047@users.noreply.github.com>
2020-03-16 16:29:29 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,69 @@
+package HomePackage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1 = 13;
+        boolean isPrime = false;
+
+        for (int i = 2; i <= num1 / 2; ++i)
+        {
+            if (num1 % i == 0)
+            {
+                isPrime = true;
+                break;
+            }
+        }
+        if (!isPrime){
+            System.out.println(num1 + " is a prime number.");}
+        else {
+            System.out.println(num1 + " is not a prime number."); }
+
+        //Second number
+        int num2 = 141;
+        int result;
+        String output, outputFinal = "Factor: ";
+        isPrime = false;
+        String[] factor = new String[10];
+
+        for (int i = 2; i <= num2 / 2; ++i)
+        {
+            if (num2 % i == 0)
+            {
+                isPrime = false;
+                factor[i]= Integer.toString(i);
+                factor[i+1]= "*";
+                result = num2 / i;
+                factor[i+2]= Integer.toString(result);
+                break;
+            }
+        }
+        if (!isPrime)
+            System.out.println(num2 + " is a not prime number.");
+        else
+            System.out.println(num2 + " is a prime number: ");
+
+        for (int i=0; i< factor.length; i++){
+
+            output = factor[i];
+
+            if ( output == null){
+           }
+            else
+                outputFinal += output;
+
+        }
+        System.out.println(outputFinal);
+
+    }
+}
+
+
+
+
+
+
